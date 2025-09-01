9b75f50ed0df8a664e9b1c07a25666a106a6ae9f
Antony Reis <antony.matheus@eyeflow.ai>
2025-08-31 23:58:01 -0300

    update: first usage of c# language

--- src/Program.cs
+++ src/Program.cs
@@ -1,3 +1,52 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("BLABLABLABLABAL");
+﻿using src.models;
+
+
+string text = "123";
+int numero_texto = Convert.ToInt32(text);
+
+string text_dois = "5210";
+
+int num = int.Parse(text_dois);
+string input = Console.ReadLine();
+
+if (input == "blablabla")
+{
+    Console.WriteLine("blablablablablablabla");
+}
+
+switch (input)
+{
+    case "a":
+    case "e":
+    case "i":
+    case "o":
+    case "u":
+        Console.WriteLine("VOGAL");
+        break;
+    default:
+        Console.WriteLine("Não é vogal");
+        break;
+}
+
+// DateTime dataAgora = DateTime.Now;
+// DateTime dataFormatada = DateTime.Now.AddDays(5);
+// String dataFormatadaString = dataFormatada.ToString("dd/MM HH:mm");
+
+
+
+// string texto = "teste";
+// int numero = 123;
+// double numero_dois = 1.23;
+// decimal preco = 1.230M;
+// bool true_ou_false = true;
+
+// Console.WriteLine($"{texto}, {numero}, {numero_dois}, {preco}, {true_ou_false}");
+
+
+// Pessoa p = new Pessoa();
+
+// p.Nome = "tunes";
+// p.Idade = 20;
+// p.Apresentar();
+
+// Console.WriteLine($"Data agora: {dataAgora}, data5dias: {dataFormatada}, data formatada: {dataFormatadaString}");
--- src/obj/Debug/net9.0/src.AssemblyInfo.cs
+++ src/obj/Debug/net9.0/src.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("src")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6ed9ba4256e3182ca35e43523a5fd5d67a734362")]
 [assembly: System.Reflection.AssemblyProductAttribute("src")]
 [assembly: System.Reflection.AssemblyTitleAttribute("src")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
