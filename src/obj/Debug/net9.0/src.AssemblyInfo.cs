33ad7bf8fddaa3f33c1d7fe6b557e1f8cf773fa2
antonyreis <antony.matheus@eyeflow.ai>
2025-09-13 15:10:23 -0300

    feat: testing c# language basic functions

--- src/obj/Debug/net9.0/src.AssemblyInfo.cs
+++ src/obj/Debug/net9.0/src.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("src")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6ed9ba4256e3182ca35e43523a5fd5d67a734362")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9b75f50ed0df8a664e9b1c07a25666a106a6ae9f")]
 [assembly: System.Reflection.AssemblyProductAttribute("src")]
 [assembly: System.Reflection.AssemblyTitleAttribute("src")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
